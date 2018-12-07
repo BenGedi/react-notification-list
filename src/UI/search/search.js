7600ccd919c70bf9543a1751f5f1795ab8d179a0
Ben <bengedi@gmail.com>
2018-12-07 14:11:57 +0200

    update styling to search on focus

--- src/UI/search/search.js
+++ src/UI/search/search.js
@@ -15,16 +15,16 @@ const StyledSearch = styled.input`
     width: 100%;
     height: 32px;
     font-size: 14px;
+    outline: none;
     line-height: 1.5;
     color: rgba(0,0,0,.65);
     background-color: #fff;
     background-image: none;
     border: 1px solid #d9d9d9;
     border-radius: 4px;
-    -webkit-transition: all .3s;
     transition: all .3s;
 
-    &:hover {
+    &:hover, &:focus {
         border-color: #40a9ff;
         border-right-width: 1px;
     }
