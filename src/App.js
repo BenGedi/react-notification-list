d15e9a1d66e61150e95c24730674a3c0dc780b82
Ben Gedi <ben.gedi@verint.com>
2018-12-07 08:29:57 +0200

    adding server, and received the data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  getNotifications = async () => {
+		const response = await window.fetch('http://localhost:3004/getNotifications');
+    const data = await response.json();
+    
+    console.log(data);
+  }
+  
+  componentDidMount() {
+		this.getNotifications();
+	}
+
+
   render() {
     return (
       <div className="App">
